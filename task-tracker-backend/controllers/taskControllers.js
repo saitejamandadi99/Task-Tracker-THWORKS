4e4a6c5490debe7535db533cdd03e79a47b8e8f9
saitejamandadi99 <saitejamandadi99>
2025-10-30 16:51:44 +0530

    added the api's for the tasks

--- task-tracker-backend/controllers/taskControllers.js
+++ task-tracker-backend/controllers/taskControllers.js
@@ -1,9 +1,62 @@
 const Task = require('../models/taskModel')
 
 //create tasks 
-
+const createTask = async (req , res) =>{
+    try {
+        const {title, description, priority,status, dueDate} = req.body
+        if(!title || !priority || !dueDate){
+            return res.status(400).json({message:'require fields title, priority, dueDate'})
+        }
+        const newTask = await Task.create(title, description, priority, status, dueDate)
+        return res.status(201).json({message:'Task created', taskId: newTask._id})
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
 //get all tasks 
-
+const getAllTasks = async (req, res)=>{
+    try {
+        const tasks = await Task.find({}).sort({createdAt: -1})     
+        if(!tasks){
+            return res.status(201).json({message:'No tasks has been created yet.'})
+        }
+        return res.status(201).json({message:'Tasks fetched successfully', tasks:tasks})
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
 //patch task on id 
+const updateTask = async (req, res)=>{
+    try {
+        const taskId = req.params.id 
+        const task =await Task.findOneAndUpdate({_id: taskId}, req.body, {new:true})
+        if (!task){
+            return res.status(404).json({message:'Task not found'})
+        }
+        return res.status(201).json({message:'task updated successfully', task:task})
+        
+    } catch (error) {
+        return res.status(500).json({message: error.message})
+    }
+}
+
+//delete Task 
+const deleteTask = async (req, res)=>{
+    try {
+        const taskId = req.params.id 
+        const task = await Task.findByIdAndDelete({_id:taskId})
+        if(!task){
+            return res.status(404).json({message:'Task not found'})
+        }
+        res.status(201).json({message:'Task deleted successfully'})
+        
+    } catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
 
 //get insights
+
+
+module.exports = {createTask, getAllTasks, updateTask, deleteTask}
